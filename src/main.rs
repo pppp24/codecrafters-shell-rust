25411d223184d80dd6e119996e9c1a068beabf7e
pppp24 <philippe.powell@gmail.com>
2026-05-04 18:02:56 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use std::io::stdin;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::{
+    env,
+    fs::{self, Metadata},
+    io::stdin,
+    os::unix::fs::PermissionsExt,
+};
+
+fn is_executable(metadata: &Metadata) -> bool {
+    let permissions = metadata.permissions();
+    // 0o111 mask checks the executable bit for owner 0o100, group 0o010, and others 0o001
+    permissions.mode() & 0o111 != 0
+}
 
 fn main() {
     loop {
@@ -25,6 +36,20 @@ fn main() {
             if rest == "exit" || rest == "echo" || rest == "type" {
                 println!("{} is a shell builtin", rest);
                 continue;
+            }
+
+            if let Some(paths) = env::var_os("PATH") {
+                for mut dir in env::split_paths(&paths) {
+                    dir.push(rest);
+
+                    if let Ok(metadata) = fs::metadata(&dir) {
+                        let is_executable = metadata.is_file() && is_executable(&metadata);
+
+                        if is_executable {
+                            println!("{} is {}", rest, dir.display());
+                        }
+                    }
+                }
             }
 
             println!("{}: not found", rest);
